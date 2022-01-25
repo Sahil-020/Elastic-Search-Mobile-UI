cefe15d6abc2871b16038c1bf926cce845163ea6
Sahil-020 <Sahil@kwiat.com>
2022-01-25 16:16:49 +0530

    updates

--- src/components/search-components/ItemTypeSearch.js
+++ src/components/search-components/ItemTypeSearch.js
@@ -17,9 +17,50 @@ class ItemTypeSearch extends Component {
         showCount={false}
         showSearch={true}
         sortBy="asc"
-        renderListItem={(label) => (
-          <div>{label !== "NULL" ? label : "None"}</div>
-        )}
+        // renderListItem={(label) => (
+        //   <div className="filter_items">
+        //     {label !== "NULL" ? label : "None"}
+        //   </div>
+        // )}
+        render={({ loading, error, data, handleChange }) => {
+          if (loading) {
+            return <div>Fetching Results.</div>;
+          }
+          if (error) {
+            return (
+              <div>
+                Something went wrong! Error details {JSON.stringify(error)}
+              </div>
+            );
+          }
+          return (
+            <div className="filter_options_container">
+              {data.map((item) => (
+                <div
+                  className="filter_options"
+                  onClick={(e) => {
+                    handleChange(item.key);
+                    e.target.classList.toggle("filter_options_active");
+                  }}
+                  key={item.key}
+                >
+                  <span
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      // if(e.target.clas)
+                      e.target.parentNode.classList.toggle(
+                        "filter_options_active"
+                      );
+                    }}
+                  >
+                    {item.key}
+                  </span>
+                  {/* <span>{item.doc_count}</span> */}
+                </div>
+              ))}
+            </div>
+          );
+        }}
         placeholder="Select Types"
       />
     );
